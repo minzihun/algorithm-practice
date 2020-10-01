ab8cc99612266a065b0f8961bef25104123e280b
Jihun Min <minzihun@gmail.com>
2020-10-01 09:51:45 +0000

    Solve the questions 1094~1096. There left still 3 questions but It's quite hard for me. So I'm gonna solve later~:))

---
+++ CodeUp/1094.py
@@ -0,0 +1,5 @@
+n = int(input())
+arr = list(map(int, input().split()))
+arr.reverse()
+
+for e in arr: print(e, end=' ')
---
+++ CodeUp/1096.py
@@ -0,0 +1,11 @@
+n = int(input())
+arr = [[0] * 19 for _ in  range(19)]
+
+for _ in range(n):
+  x, y = map(int, input().split())
+  arr[x-1][y-1] = 1
+
+for i in range(19):
+  for j in range(19):
+    print(arr[i][j], end=' ')
+  print()
