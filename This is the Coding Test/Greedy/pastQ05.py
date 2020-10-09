4a45ebc7b5111479d4c1167ca64a4f6d0595e87a
Jihun Min <minzihun@gmail.com>
2020-10-09 05:50:51 +0000

    Update pastQ05.py

--- This is the Coding Test/Greedy/pastQ05.py
+++ This is the Coding Test/Greedy/pastQ05.py
@@ -32,7 +32,7 @@ for x in data: # 각 무게에 해당하는 볼링공의 개수 카운트
 result = 0
 for i in range(1, m + 1): # 1부터 m까지의 각 무게에 대하여 처리
   n -= arr[i] # 무게가 i인 볼링공의 개수(A가 선택할 수 있는 개수) 제외
-  result += array[i] * n # B가 선택하는 경우의 수와 곱하기
+  result += arr[i] * n # B가 선택하는 경우의 수와 곱하기
 
 print(result)
 
