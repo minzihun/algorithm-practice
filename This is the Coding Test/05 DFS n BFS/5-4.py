1b25b06d43eeb7be40f0741b2456c7cec6496e42
Jihun Min <minzihun@gmail.com>
2020-10-25 08:03:21 +0000

    Create 5-4.py

---
+++ This is the Coding Test/05 DFS n BFS/5-4.py
@@ -0,0 +1,10 @@
+# 예제 5-4 재귀 함수 종료
+def recursive_function(i):
+  # 100번째 출력했을 떄 종료되도록 종료 조건 명시
+  if i == 100:
+    return
+  print(i, '번째 재귀 함수에서', i + 1, '번째 재귀 함수를 호출합니다.')
+  recursive_function(i + 1)
+  print(i, '번째 재귀 함수를 종료합니다.')
+  
+recursive_function(1)
